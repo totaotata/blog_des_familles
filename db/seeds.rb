9d2832aabcc0e1b982ace552a8830e71d1d55ed5
totaotata <totaotata@protonmail.com>
2019-01-30 15:40:41 +0100

    Les categories OK

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+User.destroy_all
+Article.destroy_all
+Category.destroy_all
+5.times do
+  category = Category.create!(name: Faker::ProgrammingLanguage.name)
+end
+10.times do
+  user = User.create!(first_name: Faker::Company.name, last_name: Faker::Company.name, email: Faker::Internet.email)
+end
+
+10.times do
+  # @random = Random.new
+  # @random = @random.rand(10)
+  article = Article.create!(title: Faker::Pokemon.name, content: Faker::ChuckNorris.fact, user_id: User.first.id, category_id: Category.last.id)
+end
